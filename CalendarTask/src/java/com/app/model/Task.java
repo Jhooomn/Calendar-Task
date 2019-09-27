30cd7646e2885bd1f61500252a8dc55229393293
Jhon Baron <jhooomn@gmail.com>
2019-09-26 19:42:52 -0500

    Task & User Classes created, already!

---
+++ CalendarTask/src/java/com/app/model/Task.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.app.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Jhon Baron
+ */
+@Entity
+public class Task implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "from")
+    private Date from;
+
+    @Column(name = "to")
+    private Date to;
+
+    @Column(name = "allDay")
+    private Boolean allDay;
+
+    public Task() {
+    }
+
+    public Task(Integer id, String title, Date from, Date to, Boolean allDay) {
+        this.id = id;
+        this.title = title;
+        this.from = from;
+        this.to = to;
+        this.allDay = allDay;
+    }
+ 
+        
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getFrom() {
+        return from;
+    }
+
+    public void setFrom(Date from) {
+        this.from = from;
+    }
+
+    public Date getTo() {
+        return to;
+    }
+
+    public void setTo(Date to) {
+        this.to = to;
+    }
+
+    public Boolean getAllDay() {
+        return allDay;
+    }
+
+    public void setAllDay(Boolean allDay) {
+        this.allDay = allDay;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Task)) {
+            return false;
+        }
+        Task other = (Task) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.app.model.Task[ id=" + id + " ]";
+    }
+
+}
