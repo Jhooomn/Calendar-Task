6797ec04a0d10079eae63252f2cf197ac02c6c23
Jhon Baron <jhooomn@gmail.com>
2019-09-29 10:12:14 -0500

    Create loginController.java
    empty

---
+++ CalendarTask/src/java/com/app/controller/loginController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.app.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Jhon Baron
+ */
+@ManagedBean
+@SessionScoped
+public class loginController {
+
+    /**
+     * Creates a new instance of loginController
+     */
+    public loginController() {
+    }
+    
+}
