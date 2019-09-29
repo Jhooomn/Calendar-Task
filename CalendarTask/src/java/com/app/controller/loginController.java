49a2586467614ae133b43a99a07049e809491617
Jhon Baron <jhooomn@gmail.com>
2019-09-29 10:54:43 -0500

    simple index front-end

--- CalendarTask/src/java/com/app/controller/loginController.java
+++ CalendarTask/src/java/com/app/controller/loginController.java
@@ -5,6 +5,10 @@
  */
 package com.app.controller;
 
+import com.app.model.User;
+import com.app.service.UserService;
+import java.util.ArrayList;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -19,7 +23,44 @@ public class loginController {
     /**
      * Creates a new instance of loginController
      */
+    
+    User user = new User();
+    List<User> users = new ArrayList<>();
+    UserService service = new UserService();
+    
     public loginController() {
+    this.userOnList();
     }
+    
+    public void userOnList(){
+        users = service.consultarTodo(User.class);
+    }
+    
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public UserService getService() {
+        return service;
+    }
+
+    public void setService(UserService service) {
+        this.service = service;
+    }
+    
+    
     
 }
