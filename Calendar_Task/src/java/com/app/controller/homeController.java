77f884cc7565e389d3dd17e6cae43259281bcae6
Jhon Baron <jhooomn@gmail.com>
2019-10-01 19:12:25 -0500

    add a contact with userid

--- Calendar_Task/src/java/com/app/controller/homeController.java
+++ Calendar_Task/src/java/com/app/controller/homeController.java
@@ -5,8 +5,13 @@
  */
 package com.app.controller;
 
+import com.app.model.Contact;
 import com.app.model.User;
+import com.app.service.ContactService;
+import com.app.service.UserService;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
@@ -27,8 +32,67 @@ public class homeController implements Serializable {
     private indexController login;
 
     private User user = new User();
+    private Contact contact = new Contact();
+
+    private ContactService contact_service = new ContactService();
+    private UserService user_service = new UserService();
+    List<Contact> contact_list = new ArrayList<>();
 
     public homeController() {
+    }
+
+    public void addContact() {
+        Contact c = contact;
+        for (User u : user_service.consultarTodo(User.class)) {
+            if ((u.getUsername().equalsIgnoreCase(login.getUser().getUsername()))) {
+                System.out.println("aqueeeeeeeee" + u.getIduser());
+                c.setIduser(u.getIduser());
+                contact_service.crear(c);
+                contact = new Contact();
+            }
+        }
+    }
+
+//    public int myId(User u) {
+//        for (User user1 : user_service.consultarTodo(User.class)) {
+//            if ((user1.getUsername().equalsIgnoreCase(u.getUsername()))
+//                    && (user1.getPhoneNumber() == u.getPhoneNumber())
+//                    && (user1.getPassword().equalsIgnoreCase(u.getPassword()))) {
+//                return user1.getIduser();
+//            }
+//        }
+//        return 0;
+//    }
+    public Contact getContact() {
+        return contact;
+    }
+
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
+
+    public UserService getUser_service() {
+        return user_service;
+    }
+
+    public void setUser_service(UserService user_service) {
+        this.user_service = user_service;
+    }
+
+    public List<Contact> getContact_list() {
+        return contact_list;
+    }
+
+    public void setContact_list(List<Contact> contact_list) {
+        this.contact_list = contact_list;
+    }
+
+    public ContactService getContact_service() {
+        return contact_service;
+    }
+
+    public void setContact_service(ContactService contact_service) {
+        this.contact_service = contact_service;
     }
 
     public indexController getLogin() {
