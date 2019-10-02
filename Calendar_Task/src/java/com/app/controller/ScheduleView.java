216344dd4ddd530e382c69d0c2d3c6361cb066e4
Jhon Baron <jhooomn@gmail.com>
2019-10-01 21:14:34 -0500

    ScheduleView

---
+++ Calendar_Task/src/java/com/app/controller/ScheduleView.java
@@ -0,0 +1,273 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.app.controller;
+
+import com.app.model.Contact;
+import com.app.model.Task;
+import com.app.model.User;
+import com.app.service.TaskService;
+import com.app.service.UserService;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.ScheduleEntryMoveEvent;
+import org.primefaces.event.ScheduleEntryResizeEvent;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.model.DefaultScheduleEvent;
+import org.primefaces.model.DefaultScheduleModel;
+import org.primefaces.model.LazyScheduleModel;
+import org.primefaces.model.ScheduleEvent;
+import org.primefaces.model.ScheduleModel;
+
+/**
+ *
+ * @author Jhon Baron
+ */
+@ManagedBean
+@RequestScoped
+public class ScheduleView implements Serializable {
+
+    @ManagedProperty(value = "#{indexController}")
+    private indexController login;
+    private List<Task> task_list = new ArrayList<>();
+    private TaskService task_service = new TaskService();
+    private UserService user_service = new UserService();
+    private ScheduleModel eventModel;
+
+    private ScheduleModel lazyEventModel;
+
+    private ScheduleEvent event = new DefaultScheduleEvent();
+
+    @PostConstruct
+    public void init() {
+        eventModel = new DefaultScheduleModel();
+//        for (Task task : task_service.consultarTodo(Task.class)) {
+//            eventModel.addEvent(new DefaultScheduleEvent(task.getTitle(), task.getFrom(), task.getTo()));
+//        }
+
+        lazyEventModel = new LazyScheduleModel() {
+
+            @Override
+            public void loadEvents(Date start, Date end) {
+                Date random = getRandomDate(start);
+                addEvent(new DefaultScheduleEvent("Lazy Event 1", random, random));
+
+                random = getRandomDate(start);
+                addEvent(new DefaultScheduleEvent("Lazy Event 2", random, random));
+            }
+        };
+    }
+
+    public Date getRandomDate(Date base) {
+        Calendar date = Calendar.getInstance();
+        date.setTime(base);
+        date.add(Calendar.DATE, ((int) (Math.random() * 30)) + 1);    //set random day of month
+
+        return date.getTime();
+    }
+
+    public Date getInitialDate() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(calendar.get(Calendar.YEAR), Calendar.FEBRUARY, calendar.get(Calendar.DATE), 0, 0, 0);
+
+        return calendar.getTime();
+    }
+
+    public ScheduleModel getEventModel() {
+        return eventModel;
+    }
+
+    public ScheduleModel getLazyEventModel() {
+        return lazyEventModel;
+    }
+
+    private Calendar today() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DATE), 0, 0, 0);
+
+        return calendar;
+    }
+
+    private Date previousDay8Pm() {
+        Calendar t = (Calendar) today().clone();
+        t.set(Calendar.AM_PM, Calendar.PM);
+        t.set(Calendar.DATE, t.get(Calendar.DATE) - 1);
+        t.set(Calendar.HOUR, 8);
+
+        return t.getTime();
+    }
+
+    private Date previousDay11Pm() {
+        Calendar t = (Calendar) today().clone();
+        t.set(Calendar.AM_PM, Calendar.PM);
+        t.set(Calendar.DATE, t.get(Calendar.DATE) - 1);
+        t.set(Calendar.HOUR, 11);
+
+        return t.getTime();
+    }
+
+    private Date today1Pm() {
+        Calendar t = (Calendar) today().clone();
+        t.set(Calendar.AM_PM, Calendar.PM);
+        t.set(Calendar.HOUR, 1);
+
+        return t.getTime();
+    }
+
+    private Date theDayAfter3Pm() {
+        Calendar t = (Calendar) today().clone();
+        t.set(Calendar.DATE, t.get(Calendar.DATE) + 2);
+        t.set(Calendar.AM_PM, Calendar.PM);
+        t.set(Calendar.HOUR, 3);
+
+        return t.getTime();
+    }
+
+    private Date today6Pm() {
+        Calendar t = (Calendar) today().clone();
+        t.set(Calendar.AM_PM, Calendar.PM);
+        t.set(Calendar.HOUR, 6);
+
+        return t.getTime();
+    }
+
+    private Date nextDay9Am() {
+        Calendar t = (Calendar) today().clone();
+        t.set(Calendar.AM_PM, Calendar.AM);
+        t.set(Calendar.DATE, t.get(Calendar.DATE) + 1);
+        t.set(Calendar.HOUR, 9);
+
+        return t.getTime();
+    }
+
+    private Date nextDay11Am() {
+        Calendar t = (Calendar) today().clone();
+        t.set(Calendar.AM_PM, Calendar.AM);
+        t.set(Calendar.DATE, t.get(Calendar.DATE) + 1);
+        t.set(Calendar.HOUR, 11);
+
+        return t.getTime();
+    }
+
+    private Date fourDaysLater3pm() {
+        Calendar t = (Calendar) today().clone();
+        t.set(Calendar.AM_PM, Calendar.PM);
+        t.set(Calendar.DATE, t.get(Calendar.DATE) + 4);
+        t.set(Calendar.HOUR, 3);
+
+        return t.getTime();
+    }
+
+    public ScheduleEvent getEvent() {
+        return event;
+    }
+
+    public void setEvent(ScheduleEvent event) {
+        this.event = event;
+    }
+
+    public void addEvent() {
+        System.out.println("entra addEvent");
+        if (event.getId() == null) {
+//            int id = 0;
+//            short allDay = 0;
+//            for (User u : user_service.consultarTodo(User.class)) {
+//                if ((u.getUsername().equalsIgnoreCase(login.getUser().getUsername()))) {
+//                    id = u.getIduser();
+//                }
+//            }
+//            Task task = new Task();
+//            task.setTitle(event.getTitle());
+//            task.setFrom(event.getStartDate());
+//            task.setTo(event.getEndDate());
+//            if (event.isAllDay()) {
+//                allDay = 1;
+//            } else {
+//                allDay = 0;
+//            }
+//            task.setAllDay(allDay);
+//            task.setIduser(id);
+//            task_service.crear(task);
+            eventModel.addEvent(event);
+        } else {
+            eventModel.updateEvent(event);
+        }
+
+        event = new DefaultScheduleEvent();
+    }
+
+    public void onEventSelect(SelectEvent selectEvent) {
+        event = (ScheduleEvent) selectEvent.getObject();
+    }
+
+    public void onDateSelect(SelectEvent selectEvent) { // Select a Specific Day
+        event = new DefaultScheduleEvent("", (Date) selectEvent.getObject(), (Date) selectEvent.getObject());
+
+    }
+
+    public void onEventMove(ScheduleEntryMoveEvent event) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event moved", "Day delta:" + event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
+
+        addMessage(message);
+    }
+
+    public void onEventResize(ScheduleEntryResizeEvent event) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event resized", "Day delta:" + event.getDayDelta() + ", Minute delta:" + event.getMinuteDelta());
+
+        addMessage(message);
+    }
+
+    private void addMessage(FacesMessage message) {
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+
+    public indexController getLogin() {
+        return login;
+    }
+
+    public void setLogin(indexController login) {
+        this.login = login;
+    }
+
+    public List<Task> getTask_list() {
+        return task_list;
+    }
+
+    public void setTask_list(List<Task> task_list) {
+        this.task_list = task_list;
+    }
+
+    public TaskService getTask_service() {
+        return task_service;
+    }
+
+    public void setTask_service(TaskService task_service) {
+        this.task_service = task_service;
+    }
+
+    public UserService getUser_service() {
+        return user_service;
+    }
+
+    public void setUser_service(UserService user_service) {
+        this.user_service = user_service;
+    }
+
+    /**
+     * Creates a new instance of ScheduleView
+     */
+    public ScheduleView() {
+    }
+
+}
